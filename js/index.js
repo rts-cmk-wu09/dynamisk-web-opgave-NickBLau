bb34b37dc3f1cba0978df3c04e78ec849535fa6e
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-02-13 08:43:13 +0100

    add variables to JS

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+let hero = document.querySelector(".hero")
+let services = document.querySelector(".services")
+let facilties = document.querySelector(".facilties")
+let sites = document.querySelector(".sites")
+let advantages= document.querySelector(".advantages")
+
 document.addEventListener("DOMContentLoaded", function() {
 
     // slå dig løs her... 
