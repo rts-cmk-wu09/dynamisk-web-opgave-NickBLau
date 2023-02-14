2620e39009ea68674efb1af9cb2a60d708de2754
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-02-14 09:36:26 +0100

    add change to hero article height

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-let hero = document.querySelector(".hero")
-let services = document.querySelector(".services")
-let facilties = document.querySelector(".facilties")
-let sites = document.querySelector(".sites")
-let advantages= document.querySelector(".advantages")
+let heroSection = document.querySelector(".hero")
+let servicesSection = document.querySelector(".services")
+let faciltiesSection = document.querySelector(".facilties")
+let sitesSection = document.querySelector(".sites")
+let advantagesSection = document.querySelector(".advantages")
 
 document.addEventListener("DOMContentLoaded", function() {
 
     // slå dig løs her... 
 
+
     //eksempel på at udskrive alle overskrifter i services i konsollen:
-    services.forEach(service => console.log(service.headline))
 
 }) // DOMContentLoaded slut
